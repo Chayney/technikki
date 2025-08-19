b3f9dd2b0e8c2e2f8a3980b4f30cae65ddfbaec1
Chayney <chunchunmaru33333@gmail.com>
2025-08-19 21:28:13 +0900

    ロゴの作成

--- components/atoms/Logo/index.tsx
+++ components/atoms/Logo/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Link from 'next/link';
 
 type LogoProps = {
-    label: string
+    label: React.ReactNode
 };
 
 export const Logo: React.FC<LogoProps> = ({ label }) => (
     <Link href="/">
-        <span>{label}</span>
+        <span style={{ lineHeight: 1.2 }}>{label}</span>
     </Link>
 );
