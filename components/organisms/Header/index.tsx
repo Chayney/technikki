237f9dbc083edbf8d2d54effe622577ac973df53
Chayney <chunchunmaru33333@gmail.com>
2025-08-19 21:29:12 +0900

    ヘッダーの作成

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -1,10 +1,34 @@
+import { Logout } from '../../atoms/Link/logout';
+import { Home } from '../../atoms/Link/home';
+import { Profile } from '../../atoms/Link/profile';
+import { About } from '../../atoms/Link/about';
 import { Logo } from '../../atoms/Logo/index';
+import { useSession } from "next-auth/react";
 import styles from './style.module.css';
 
 export default function Header() {
+    const { data: session } = useSession();
     return (
-        <header className={styles.logo}>
-            <Logo label="テク日記" />
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <Logo
+                    label={
+                        <>
+                            <span className={styles.mainTitle}>テク日記</span>
+                            <br />
+                            <span className={styles.subTitle}>～かけだしエンジニアの技術日記～</span>
+                        </>
+                    }
+                />
+            </div>
+            <div className={styles.nav}>
+                <Home label="ホーム" />
+                <About label="このブログについて" />
+                <Profile label="プロフィール" />
+                {session && (
+                    <Logout label="ログアウト" />
+                )}
+            </div>
         </header>
     );
 }
