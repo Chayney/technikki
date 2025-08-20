2e7419167cdbfc8602224d82b7dfdab5141028bb
Chayney <chunchunmaru33333@gmail.com>
2025-08-21 00:19:57 +0900

    ログアウトの作成

--- components/atoms/Link/logout.tsx
+++ components/atoms/Link/logout.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { signOut } from "next-auth/react";
+import styles from './logout.module.css';
 
 type LogoutProps = {
     label: string
 };
 
 export const Logout: React.FC<LogoutProps> = ({ label }) => (
-    <button onClick={() => signOut({ callbackUrl: "/" })}>
+    <button className={styles.logout} onClick={() => signOut({ callbackUrl: "/" })}>
         {label}
     </button>
 );
