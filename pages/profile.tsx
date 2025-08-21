680a83b0616f3ea5d34e11aae428bb47de09d9c4
Chayney <chunchunmaru33333@gmail.com>
2025-08-21 23:36:03 +0900

    レイアウト修正

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,16 +1,9 @@
-import { useSession, signOut } from "next-auth/react";
 import { Header } from "../shared/header/components/header";
 
 export default function Profile() {
-    const { data: session } = useSession();
     return (
         <div>
             <Header />
-            {session && (
-                <button onClick={() => signOut({ callbackUrl: "/" })}>
-                    ログアウト
-                </button>
-            )}
         </div>
     )
 }
